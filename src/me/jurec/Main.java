2c5a12c715f426a2ceb96a5ff383ae8775629325
Julian Jurec <jjurec@m.spyro-soft.com>
2018-02-27 22:09:06 +0100

    Added best editor twice

--- src/me/jurec/Main.java
+++ src/me/jurec/Main.java
@@ -3,10 +3,11 @@ package me.jurec;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.println("Emacs still best");
         printTheBestEditor();
     }
 
     private static void printTheBestEditor() {
-        //todo: Implement
+        System.out.println("Emacs");
     }
 }
