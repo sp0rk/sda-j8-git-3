0f0f15c984c613ce40022d2539bc27510e2407d5
Julian Jurec <jjurec@m.spyro-soft.com>
2018-02-27 22:06:13 +0100

    Added task

---
+++ src/me/jurec/Main.java
@@ -0,0 +1,12 @@
+package me.jurec;
+
+public class Main {
+
+    public static void main(String[] args) {
+        printTheBestEditor();
+    }
+
+    private static void printTheBestEditor() {
+        //todo: Implement
+    }
+}
